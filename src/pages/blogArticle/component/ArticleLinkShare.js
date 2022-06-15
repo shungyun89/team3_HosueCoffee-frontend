9c872f7c1eebc61b2433c6ecc627567302173260
shungyun89 <94456518+shungyun89@users.noreply.github.com>
2022-06-15 16:14:36 +0800

    aaaa

--- src/pages/blogArticle/component/ArticleLinkShare.js
+++ src/pages/blogArticle/component/ArticleLinkShare.js
@@ -19,7 +19,7 @@ import instagramImg from '../img/share btn ig.svg'
 
 const ArticleLinkShare = (props) => {
   const {thisBid}=props
-  const shareUrl =`https://hosue-coffee-backend.herokuapp.com/blog/blogArticle?blog_id=${thisBid}`;
+  const shareUrl =`https://team3-hosue-coffee-frontend.vercel.app/blog/blogArticle?blog_id=${thisBid}`;
 
   
   return (
